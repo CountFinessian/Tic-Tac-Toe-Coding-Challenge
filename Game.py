3409fff2b493572ddf537ed2d09326c5dfdee187
Jacob Williams <jawoba004@gmail.com>
2024-08-08 12:59:57 -0700

    third hour of programming done.

--- Game.py
+++ Game.py
@@ -1,31 +1,21 @@
+from Mechanics import playgame
+
 print( "Welcome to my game of Tic Tac Toe!" )
 input("Press the Enter key to continue: ") 
 
-def gameturn (turn):
-    q = "1"
-    w = "2"
-    e = "3"
-    r = "4"
-    t = "5"
-    y = "6"
-    u = "7"
-    i = "8"
-    o = "9"
 
-    print ( q + "|" + w + "|" + e )
-    print ( r + "|" + t + "|" + y )
-    print ( u + "|" + i + "|" + o )
-    
 x = True
 while x:
-    print(" ")
-    turn = input ( "enter 1 to go first or 2 to go second \n" )
     
-    if turn == "1":
+    turn = input ( "enter 0 to go first or 1 to go second: " )
+    
+    if turn == "0":
         print( "\nYou are X, you will go first" )
         x = False
-    elif turn == "2":
+        playgame(0)
+    elif turn == "1":
         print ( "\nYou are O, you will go second")
         x = False
-        gameturn(turn)
+        playgame(1)
 
+# end of game to delcare a winner or ask to play again.
--- Mechanics.py
+++ Mechanics.py
@@ -1,33 +1,53 @@
+import random
+
 # have a board which is just an array
-q = "1"
-w = "2"
-e = "3"
-r = "4"
-t = "5"
-y = "6"
-u = "7"
-i = "8"
-o = "9"
+movesToMake = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+gameBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-movesToMake = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-gameboard = (q, w, e, r, t, y, u, i, o)
-
-def printboard ( gameboard ):
-    print ( gameboard[0] + "|" + gameboard[1] + "|" + gameboard[2] )
-    print ( gameboard[3] + "|" + gameboard[4] + "|" + gameboard[5] )
-    print ( gameboard[6] + "|" + gameboard[7] + "|" + gameboard[8] )
+def printboard ( gameBoard ):
+    print ( gameBoard[0] + "|" + gameBoard[1] + "|" + gameBoard[2] )
+    print ( gameBoard[3] + "|" + gameBoard[4] + "|" + gameBoard[5] )
+    print ( gameBoard[6] + "|" + gameBoard[7] + "|" + gameBoard[8] + "\n")
 
 
-def computer( movesToMake ):
-    pass
+def computer( player ):
+    # randomly choose a move inside of movesToMake
+    # remove it from the movesToMake and add it to the gameBoard
+    randomMove = random.choice(movesToMake)
+    movesToMake.remove(randomMove)
+    gameBoard[randomMove - 1] = player
+    print ("The computer made its move\n")
 
+def maketurn ( player ):
+    x = True
+    while x:
+        printboard (gameBoard)
+        position = input ( "Please enter the move corresponding to the number on the board: " )
+
+        try:
+            position = int(position)
+            if position in movesToMake:
+                movesToMake.remove(position)
+                gameBoard[position - 1] = player
+                x = False
+            else:
+                print ("Place is already taken, try again.")
+
+        except ValueError:
+            print("Error: Cannot convert", position, "to an integer")
+            
 def playgame (turn):
     # mechanics to make the turn happen
-
-    if turn:
-        computer ( turn )
-    else:
-        print ( " please enter the move you'd like to make (1-9)")
-        printboard (gameboard)
-        computer ( turn )
+    x = True
+    while x:
     
+        if turn == 0:
+            maketurn ( "X" )
+            computer ( "O" )
+
+        elif turn == 1:
+            computer ( "X" )
+            maketurn ( "O" )
+    
+    # needs to check at the end of one of the critera to end the game is met 
+    # returns the neccessary winner (player X or player O)
